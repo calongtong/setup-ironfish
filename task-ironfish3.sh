6b27eb684d1310698340399803e2ecbf98add2f8
calongtong <47720318+calongtong@users.noreply.github.com>
2023-02-12 04:55:13 +0700

    Update task-ironfish3.sh

--- task-ironfish3.sh
+++ task-ironfish3.sh
@@ -8,11 +8,11 @@ echo -e "MINT asset with name: $IRONFISH_WALLET\n"
 echo -e "========================================\n"
 
 
-#month=$(date +"%m")
-#day=$(date +"%d")
-#hour=$(date +"%H")
-#meta_data="metadata $month $day $hour"
-meta_data="meta data test"
+month=$(date +"%m")
+day=$(date +"%d")
+hour=$(date +"%H")
+meta_data="metadata $month $day $hour"
+#meta_data="meta data test"
 echo -e "$meta_data \n"
 #output_mint=$($(which ironfish) wallet:mint --name $IRONFISH_WALLET --metadata "$meta_data" --amount 10 --fee 0.0000001 --confirm)
 #echo $output_mint
@@ -60,17 +60,18 @@ do
 done
 
 
+
 #======================================================================
-# Burn Assets
+# Send And Burn Assets
 #======================================================================
 #ironfish wallet:burn --assetId 88232d14fedbe91bd3c98013e2807ca643f0bd896e566b435cc957514df2c872 --amount 0.1 --fee 0.000001 --confirm
 . $HOME/.bash_profile
 echo -e "========================================\n"
-echo -e "Burn asset\n"
+echo -e "Send And Burn asset\n"
 echo -e "========================================\n"
 # Get balances 
-balance=$($(which ironfish) wallet:balances | grep $IRONFISH_WALLET | head -1 | awk '{print $3}')
-asset_id=$($(which ironfish) wallet:balances | grep $IRONFISH_WALLET | head -1 | awk '{print $2}')
+balance=$($(which ironfish) wallet:balances | grep $IRONFISH_WALLET | grep "10.00000000" | head -1 | awk '{print $3}')
+asset_id=$($(which ironfish) wallet:balances | grep $IRONFISH_WALLET | grep "10.00000000" | head -1 | awk '{print $2}')
 echo -e "$asset_id --> $balance\n"
 
 minimum_balance=2
@@ -78,6 +79,33 @@ if [[ $(echo "$balance < $minimum_balance" | bc -l) -eq 1 ]]; then
 	echo "The balance is below the minimum threshold"
 else
 	echo -e "The balance of $IRONFISH_WALLET is sufficient\n"
+	
+	echo -e "----------------\n"
+	echo -e "Send asset: $asset_id\n"
+	echo -e "----------------\n"
+	output_send_2=$($(which ironfish) wallet:send --assetId=$asset_id --amount 1 --fee 0.0000001 --to $send_to --confirm)
+	echo $output_send_2
+
+	tx_hash_send_2=$(echo "$output_send_2" | grep "Transaction Hash:" | cut -d ' ' -f 3)
+	echo -e "Transaction Hash: $tx_hash_send_2\n"
+	echo -e "Wait a few minutes for transaction confirm ...\n"
+	sleep 30
+
+	# tracking transaction status
+	tx_status_send_2=$($(which ironfish) wallet:transactions | grep $tx_hash_send_2 | awk '{print $4}')
+
+	while [ "$tx_status_send_2" != "confirmed" ]
+	do
+		tx_status_send_2=$($(which ironfish) wallet:transactions | grep $tx_hash_send_2 | awk '{print $4}')
+		echo -e "$tx_hash_send_2 --> $tx_status_send_2\n"    
+		sleep 30	
+	done
+	
+	
+	
+	echo -e "----------------\n"
+	echo -e "Burn asset\n"
+	echo -e "----------------\n"
 	
 	output_burn=$($(which ironfish) wallet:burn --assetId $asset_id --amount 1 --fee 0.000001 --confirm)
 	tx_hash_burn=$(echo "$output_burn" | grep "Transaction Hash:" | cut -d ' ' -f 3)
